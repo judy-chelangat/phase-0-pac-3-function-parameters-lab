a461935c183db7b1f44a3f8694b0e129b6b464e5
judy-chelangat <judysigilai75@gmail.com>
2023-06-10 15:35:08 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-function introduction(name){
-return `Hi, my name is ${name}.`
-}
-function introductionWithLanguage(name,language){
-    return `Hi, my name is ${name} and I am learning to program in ${language}.`
-}
-introductionWithLanguage("judy","javascript");
+// function introduction(name){
+// return `Hi, my name is ${name}.`
+// }
+// function introductionWithLanguage(name,language){
+//     return `Hi, my name is ${name} and I am learning to program in ${language}.`
+// }
+// introductionWithLanguage("judy","javascript");
 
-function introductionWithLanguageOptional(name, language="JavaScript"){
-return `Hi, my name is ${name} and I am learning to program in ${language}.`
+// function introductionWithLanguageOptional(name, language="JavaScript"){
+// return `Hi, my name is ${name} and I am learning to program in ${language}.`
+// }
+// introductionWithLanguageOptional("judy", "Python")
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
 }
-introductionWithLanguageOptional("judy", "Python")
+function introductionWithLanguageOptional(name,language="JavaScript"){
+ return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+function introduction(name)
+{
+    return `Hi, my name is ${name}.`
+}
